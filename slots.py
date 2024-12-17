69aa5ad55e332d4d88eb4b3f468763a7fc9dd4c1
EdmondZhao-o <EDmondz3@nycstudents.net>
2024-12-17 10:16:31 -0500

    Update slots.py
    wda

--- slots.py
+++ slots.py
@@ -6,8 +6,8 @@ class Slots:
     pygame.init()
 
     black = (0, 0, 0)
-    green = (0, 255, 0)  
-    blue = (0, 0, 255)    
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
     orange = (255, 165, 0)
     white = (255, 255, 255)
     red = (255, 0, 0)
@@ -23,9 +23,9 @@ class Slots:
         text = self.font.render('SPACE TO ROLL', True, self.green)
         slot1 = self.font.render('', True, self.black)
         slot2 = self.font.render('', True, self.black)
-        slot3 = self.font.render('', True, self.black)  
+        slot3 = self.font.render('', True, self.black)
         cashText = self.font.render(f"Money: ${self.money}", True, self.black)
-        textRect = text.get_rect() 
+        textRect = text.get_rect()
         slot1Rect = slot1.get_rect()
         slot2Rect = slot2.get_rect()
         slot3Rect = slot3.get_rect()
@@ -36,7 +36,7 @@ class Slots:
         slot3Rect.center = (500, 300)
         cashTextRect.center = (100, 100)
         return text, slot1, slot2, slot3, cashText, slot1Rect, slot2Rect, slot3Rect, textRect, cashTextRect
-    
+
     def roll(self):
         x = random.randint(0, 4)
         y = random.randint(0, 4)
@@ -46,17 +46,24 @@ class Slots:
         slot3 = self.font.render(self.fruits[z], True, self.color[z])
         return slot1, slot2, slot3, x, y, z
 
+    def calculate_money(self, x, y, z):
+        if x == y == z:
+            return self.bet * 2
+        elif x == y or y == z or x == z:
+            return self.bet * 0.5
+        else:
+            return -self.bet
+
     def game_loop(self):
         rolling = False
         roll_start_time = 0
-        total_roll_time = 300
+        total_roll_time = 200
         roll_count = 0
-    def no_money(self):
 
         text, slot1, slot2, slot3, cashText, slot1Rect, slot2Rect, slot3Rect, textRect, cashTextRect = self.starting()
 
         while True:
-            if self.money>=self.bet:
+            if self.money >= self.bet:
                 self.screen.fill(self.white)
                 self.screen.blit(text, textRect)
                 self.screen.blit(slot1, slot1Rect)
@@ -70,16 +77,8 @@ class Slots:
                         slot1, slot2, slot3, x, y, z = self.roll()
                     else:
                         rolling = False
-                        if x == y == z:  
-                            self.money += self.bet * 2  
-                        elif x == y or y == z or x == z:
-                            self.money += self.bet * 0.5
-                        else:
-                            self.money -= self.bet
-                        roll_count += 1
-                        if roll_count >= 10:
-                            rolling = False
-                            roll_count = 0
+                        profit = self.calculate_money(x, y, z)
+                        self.money += profit
 
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -89,8 +88,23 @@ class Slots:
                         if not rolling:
                             rolling = True
                             roll_start_time = pygame.time.get_ticks()
-            else: 
+					""" if event.type == pygame.KEYDOWN and event.key == pygame.K_ESC:
+						pygame.quit()
+						quit() """
+
+            else:
+                game_over_text = self.font.render("GAME OVER", True, self.red)
+                game_over_rect = game_over_text.get_rect()
+                game_over_rect.center = (300, 300)
+                self.screen.fill(self.white)
+                self.screen.blit(game_over_text, game_over_rect)
+                pygame.display.update()
+                pygame.time.wait(3000)
+                pygame.quit()
+                quit()
+
             pygame.display.update()
+
 
 game = Slots()
 game.game_loop()
