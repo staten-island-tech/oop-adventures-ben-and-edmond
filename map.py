24fe4ed1807975dfead6de494e960589a98e8a1f
EdmondZhao-o <EDmondz3@nycstudents.net>
2025-01-13 10:15:03 -0500

    grammy location

--- map.py
+++ map.py
@@ -11,12 +11,14 @@ class Game:
         self.player_pos = [5, 8]
         self.money = money
         self.slot_machine_locations = [[2, 6], [2, 7], [2, 8], [2, 9], [2, 10],
-                                       [9, 6], [9, 7], [9, 8], [9, 9], [9, 10],
-                                       [4, 6], [4, 7], [4, 8], [4, 9], [4, 10],
+                                       [9, 6], [9, 7], [9, 8], [9, 9], [9, 10], 
+                                       [4, 7], [4, 8], [4, 9], [4, 10],
                                        [7, 6], [7, 7], [7, 8], [7, 9], [7, 10]]
         self.blackjack_locations = [ [2, 15], [3, 15], [4, 15], [2, 13], [3, 13], [4, 13], [7,15] , [8,15] , [9,15] , [7,13] , [8,13] , [9,13] ]
         self.roulette_locations = [[2, 1], [3, 1], [4, 1], [2, 3], [3, 3], [4, 3], [7,1] , [8,1] , [9,1] , [7,3] , [8,3] , [9,3]]
         self.snail_locations = [[0,0],[11,0],[11,17],[0,17],[2,4],[2,5],[2,11],[2,12]]
+        self.grammy_locations = [[5,6]]
+        self.grammy_machine_location = [[4,6]]
         self.map = self.create_map()
 
     def create_map(self):
@@ -43,7 +45,9 @@ class Game:
         for snail_pos in self.snail_locations:
             sx, sy = snail_pos
             self.map[sx][sy] = " 🐌"
-
+        for grammy_pos in self.grammy_locations
+            gx, gy = grammy_pos
+            self.map[gx][gy] = " 👵"
         for row in self.map:
             print("".join(row))
 
@@ -91,3 +95,14 @@ class Game:
             elif move in ["w", "a", "s", "d"]:
                 self.move_player(move)
                 self.check_position()
+    class Person:
+        def __init__(self, fname, lname):
+            self.firstname = fname
+            self.lastname = lname
+
+        def printname(self):
+            print(self.firstname, self.lastname)
+    class grammy:
+        def __init__(self, fname, lname):
+            super().__init__(fname, lname)
+        
